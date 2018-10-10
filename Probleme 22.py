f6dc4f51f5f5df9242569b81bae588c9a9881571
theoleleu <43882382+theoleleu@users.noreply.github.com>
2018-10-10 14:59:29 +0200

    Update Probleme 22.py

--- Probleme 22.py
+++ Probleme 22.py
@@ -5,11 +5,12 @@ def solve(n=0):
     S=0
     Val=0
     texte=texte[1:len(texte)-1]
-    T=sorted(texte.split('","'),key=str)
-    for k in T:
+    Texte=sorted(texte.split('","'),key=str)
+    for prenom in Texte:
         Nieme+=1
-        for i in k:
+        for lettre in prenom:
             Val+=ord(i)-64
         S+=Val*Nieme
         Val=0
     return S
+print(solve())
